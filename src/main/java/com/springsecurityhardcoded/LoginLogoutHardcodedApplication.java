84f0f14b705e0baec47b851fd9948bdc9bef0d36
Srishti-Mallick <srishtimallick14@gmail.com>
2020-04-06 16:20:16 +0530

    first commit

---
+++ src/main/java/com/springsecurityhardcoded/LoginLogoutHardcodedApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurityhardcoded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginLogoutHardcodedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginLogoutHardcodedApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/springsecurityhardcoded/welcome/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.springsecurityhardcoded.welcome;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "Welcome";
+	}
+}
